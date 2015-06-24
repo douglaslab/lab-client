18c7c0503aa7ff9a0ed753973a56e5814700fb21
Guy Vider <guy@guyvider.com>
2015-06-24 09:16:51 -0700

    streamlined error handling

--- app.js
+++ app.js
@@ -52,37 +52,20 @@ app.use('/users', userRoutes(passport));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Page Not Found (' + req.url + ')');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-if (app.get('env') === 'development') {
-  // development error handler - will print stacktrace
-  app.use(function(err, req, res, next) { // eslint-disable-line no-unused-vars
-    console.log(req.user);
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err,
-      username: (req.user && req.user.name) || '',
-      permissionLevel: (req.user && req.user.permissionLevel) || ''
-    });
+// error handler
+app.use(function(err, req, res, next) { // eslint-disable-line no-unused-vars
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: (app.get('env') === 'development') ? err : {},
+    username: (req.user && req.user.name) || '',
+    permissionLevel: (req.user && req.user.permissionLevel) || ''
   });
-}
-else {
-  // production error handler - no stacktraces leaked to user
-  app.use(function(err, req, res, next) { // eslint-disable-line no-unused-vars
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: {},
-      username: (req.user && req.user.name) || '',
-      permissionLevel: (req.user && req.user.permissionLevel) || ''
-    });
-  });
-}
+});
 
 module.exports = app;
